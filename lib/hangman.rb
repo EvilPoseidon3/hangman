abcb58e3990037419a928850a45deefbd861fa16
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-06-03 14:17:31 -0500

    completed iteration of hangman added colorize gem

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,37 @@
 require 'csv'
+require 'colorized_string'
+# require_relative 
+
+require_relative('sort/guessalgo')
+require_relative('sort/hangmanpicture')
 
 
 text_document_lines = File.readlines('google-10000-english-no-swears.txt')
+
 word_array = Array.new
+
 text_document_lines.each do |word|
   if word.strip.length.between?(5,12)
     word_array.push(word.strip)
   end
 end
-puts word_array
+
+def random_word_pick(word_array)
+  return word_array[rand(0..word_array.length)]
+end
+
+def game_over_display(game)
+  game.displayboard()
+  puts ColorizedString["           Game Over"].colorize(:color => :light_red, :mode => :blink_slow)
+  puts ColorizedString["        Thanks for Playing"].colorize(:color => :green, :mode => :underline)
+end
+#create the game  on intitial run
+inital_game = Hangmanpicture.new(random_word_pick(word_array))
+#take that created game object and run it through a loop in the Guess class
+
+while inital_game.gameover == false do 
+ Guess.new(inital_game)
+ if inital_game.gameover == true
+  game_over_display(inital_game)
+end
+end
